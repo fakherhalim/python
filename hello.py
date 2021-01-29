3ea7222bc8756f207e4917b6d7daf863da692998
Fakher Halim <fakher@tpgxml.com>
2021-01-28 22:06:21 -0600

    Quick plotting intital commit

---
+++ hello.py
@@ -0,0 +1,9 @@
+""" A first plot - get some numbers, plot them against
+a uniform X axis starting at 1 using all the defaults."""
+
+import matplotlib.pyplot as plt
+
+plt.plot([1,23,2,4])
+plt.ylabel('some numbers')
+
+plt.show()
