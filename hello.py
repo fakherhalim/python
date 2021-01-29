581be668034734808b23e71cdaef99217ca97ea6
Fakher Halim <fakher@tpgxml.com>
2021-01-28 22:13:23 -0600

    Added comment for matplotlib installation

--- hello.py
+++ hello.py
@@ -1,5 +1,9 @@
 """ A first plot - get some numbers, plot them against
-a uniform X axis starting at 1 using all the defaults."""
+a uniform X axis starting at 1 using all the defaults.
+Make sure you do: 
+    sudo pip3 install matplotlib
+
+"""
 
 import matplotlib.pyplot as plt
 
